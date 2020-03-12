0e70bd8d76322dc2fa7158c798b49c8cd1d0cffa
Romulo Soares <romulosoares@ice.ufjf.br>
2020-03-11 23:40:12 -0300

    Adicionado a chamada da classes de organização no main. Criação de um menu para a seleção do método de organização

--- src/trabalho/Trabalho.java
+++ src/trabalho/Trabalho.java
@@ -5,6 +5,8 @@ package trabalho;
 
 import java.io.IOException;
 import java.util.Scanner;
+import ordenacao.InsertionSort;
+import ordenacao.MergeSort;
 
 public class Trabalho {
     public static void main(String[] args) throws IOException {
@@ -12,9 +14,26 @@ public class Trabalho {
         Scanner input = new Scanner(System.in);
         Registro[] vetor; //Inicia um vetor de Musica para armazernar os dados do arquivo .csv
         Leitura a = new Leitura(); //Inicializa a classe para executar a leitura do arquivo .csv
-
+        InsertionSort insertion;
+        MergeSort merge;
+        
         vetor = a.lerArquivo(); //Lê o arquivo .csv e passa para o vetor os dados
-            
+        //Menu para selecionar o método de ordenção
+        System.out.println("Escolha como orgazinar");
+        System.out.println("(1) Insertion Sort");
+        System.out.println("(2) Merge Sort");
+        option = input.nextInt();
+        switch(option){
+            case 1: insertion = new InsertionSort(vetor, vetor.length);
+                    insertion.organiza();
+                    break;
+            case 2: merge = new MergeSort(vetor);
+                    merge.organiza();
+                    break;
+        }
+        //Final do menu
+
+        //Impressão dos dados após a organização
         for (int i = 0; i < vetor.length; i++) {
             vetor[i].imprimir();
         }
