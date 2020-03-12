28ec0f1a3644ce6b0bbffd5b4badf7aa9281f6f1
Romulo Soares <romulosoares@ice.ufjf.br>
2020-03-11 23:07:47 -0300

    Contrutor, gets, set e função para impressão criados

--- src/trabalho/Registro.java
+++ src/trabalho/Registro.java
@@ -1,5 +1,37 @@
 package trabalho;
 
 public class Registro {
-
+private String id;
+    private String name;
+    private String artist;
+    //Construtor
+    public Registro(String id, String nome, String artist){
+        this.id = id;
+        this.name = nome;
+        this.artist = artist;
+    }
+    //Gets
+    public String getArtist() {
+        return artist;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    //Sets
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    //Função para imprimir os dados
+    public void imprimir(){
+        System.out.println(this.id + "|" + this.name + "|" + this.artist);
+    }
 }
