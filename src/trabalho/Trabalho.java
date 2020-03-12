b35b1b82236ad715b3f8a315a9d89755fb3f9d5c
Romulo Soares <romulosoares@ice.ufjf.br>
2020-03-11 23:21:40 -0300

    Código no main adicionados para efetuar a leitura e impressão dos dados

--- src/trabalho/Trabalho.java
+++ src/trabalho/Trabalho.java
@@ -3,8 +3,21 @@
  */
 package trabalho;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Trabalho {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int option;
+        Scanner input = new Scanner(System.in);
+        Registro[] vetor; //Inicia um vetor de Musica para armazernar os dados do arquivo .csv
+        Leitura a = new Leitura(); //Inicializa a classe para executar a leitura do arquivo .csv
+
+        vetor = a.lerArquivo(); //Lê o arquivo .csv e passa para o vetor os dados
+            
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i].imprimir();
+        }
         
     }  
 }
