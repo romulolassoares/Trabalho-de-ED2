ede1fde311482d3c3c647d2261bb90ec736922b5
Romulo Soares <romulosoares@ice.ufjf.br>
2020-03-21 07:59:57 -0300

    Funções responsáceis pela ordenação adicionadas na classe QuickSort

--- src/ordenacao/QuickSort.java
+++ src/ordenacao/QuickSort.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ordenacao;
 
-/**
- *
- * @author romulo
- */
+import trabalho.Registro;
+
 public class QuickSort {
+    Registro[] vet;
+
+    public QuickSort(Registro[] vet) {
+        this.vet = vet;
+    }
     
+    public void organiza(){
+        quickSort(0, this.vet.length-1);
+    }
+    
+    protected void  quickSort(int esq, int dir){
+        if(esq < dir){
+            int pivo = particiona(esq, dir);
+            quickSort(esq, pivo-1);
+            quickSort(pivo+1, dir);
+        }
+    }
+    
+    protected int particiona(int esq, int dir){
+        Registro pivo = this.vet[esq];
+        int i = (esq + 1), j = dir;
+        while (i <= j){
+            if (this.vet[i].getArtist().compareToIgnoreCase(pivo.getArtist()) <= 0){
+                i++;
+            }
+            else if (this.vet[j].getArtist().compareToIgnoreCase(pivo.getArtist()) > 0){
+                j--;
+            }
+            else {
+                Registro temp = this.vet[i];
+                this.vet[i] = this.vet[j];
+                this.vet[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        this.vet[esq] = this.vet[j];
+        this.vet[j] = pivo;
+        return j;
+    }
 }
